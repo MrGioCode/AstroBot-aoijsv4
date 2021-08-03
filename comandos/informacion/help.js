fb9d6c776792088a74d2f59e301e710a78357e05
TheGioXD <83745961+TheGioXD@users.noreply.github.com>
2021-08-03 12:38:06 -0700

    Create help.js

---
+++ comandos/informacion/help.js
@@ -0,0 +1,11 @@
+module.exports = ({
+     name: "help",
+     code: `
+$reply[$messageID;{author:Ayuda en el bot:$authorAvatar}{description:
+──────「Español」──────
+> Seguro estaras interesado en ver los comandos de nuestro gran bot xAstroBot, bueno, muy sencillo!, usa \`!comandos\` para ver todos los comandos del bot, pero antes tendras que activar las categorias usando \`!activar [informacion/automoderacion/configuracion/moderacion/bienvenidas/despedidas]\`
+──────「English」──────
+> Sure you will be interested in seeing the commands of our great bot xAstroBot, well, very simple!, use \`!comandos\` to see all the bot commands, but first you must activate the categories of the bot using \`!activar [categoria]\`
+
+<:link:856765531382218772> | [invitacion](https://discord.com/api/oauth2/authorize?client_id=864965981290102784&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.gg%2FkHv3dV3KeW&scope=bot) | [soporte](https://discord.gg/mKQGB6Q2VP) |}{thumbnail:$userAvatar[$clientID]}{footer:.:$authorAvatar}{color:BLUE};no]`
+})
