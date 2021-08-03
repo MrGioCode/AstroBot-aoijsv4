dd83e813cad7b594644b64d3dc88a9d890e91fcf
TheGioXD <83745961+TheGioXD@users.noreply.github.com>
2021-08-03 12:42:35 -0700

    Create nuke.js

---
+++ comandos/moderacion/nuke.js
@@ -0,0 +1,11 @@
+module.exports = ({
+  name: "nuke",
+  code: `
+ $deleteChannels[$channelID]
+ $channelSendMessage[$cloneChannel[$channelID;yes];<:tick:856950258219352075> | \`$channelName[$channelID]\` Explotado correctamente]
+$onlyPerms[managechannels;<:cross_red:856744445395206144> | No cuentas con los permisos necesarios]
+$onlyBotPerms[managechannels;<:cross_red:856744445395206144> | No cuento con los permisos necesarios]
+$suppressErrors[<:cross_red:856744445395206144> | Error al intentar explotar este canal]
+$cooldown[3s;<:cross_red:856744445395206144> | Tranquilo...]
+$onlyIf[$getServerVar[moderacion]==true;<:cross_red:856744445395206144> | Los comandos \`moderacion\` no estan activados. Usa: !activar moderacion]`
+});
