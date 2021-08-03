7e727b131880c1faf18e36bec9979dd8ad90d99a
TheGioXD <83745961+TheGioXD@users.noreply.github.com>
2021-08-03 12:44:42 -0700

    Create update.js

---
+++ comandos/owner/update.js
@@ -0,0 +1,7 @@
+module.exports = ({
+    name: "actualizar-comandos",
+    code: `
+$customEmoji[cargando_rainbow] | \`Cargando comandos...\` $editIn[3s;<:tick:856950258219352075> | Hecho, comandos actualizados correctamente!]
+$updateCommands
+$onlyForIDs[$botOwnerID;<:cross_red:856744445395206144> | Solo dueño del bot]`
+})
