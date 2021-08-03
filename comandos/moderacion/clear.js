5e7af0b1a0c43a5c3bc1c79a79993644ddcba055
TheGioXD <83745961+TheGioXD@users.noreply.github.com>
2021-08-03 12:41:34 -0700

    Create clear.js

---
+++ comandos/moderacion/clear.js
@@ -0,0 +1,16 @@
+module.exports = ({
+name: "clear",
+aliases: ['purge'],
+code: `
+<:tick:856950258219352075> | He eliminado \`$clear[$message[1];everyone;$channelID;yes] mensajes\`
+$deleteIn[2s]
+$wait[200]
+$deletecommand
+$onlyIf[$message[1]<=100;<:cross_red:856744445395206144> | Lá cantidad no puede exceder de los 100 mensajes]
+$onlyIf[$isNumber[$message]==true;<:cross_red:856744445395206144> | Numero no proporcionado]
+$argsCheck[>1;<:cross_red:856744445395206144> | No haz colocado un numero de mensajes]
+$onlyPerms[managechannels;<:cross_red:856744445395206144> | No cuentas con los permisos necesarios]
+$onlyBotPerms[managechannels;<:cross_red:856744445395206144> | No cuento con los permisos necesarios]
+$cooldown[3s;<:cross_red:856744445395206144> | Tranquilo...]
+$onlyIf[$getServerVar[moderacion]==true;<:cross_red:856744445395206144> | Los comandos \`moderacion\` no estan activados. Usa: !activar moderacion]`
+});
