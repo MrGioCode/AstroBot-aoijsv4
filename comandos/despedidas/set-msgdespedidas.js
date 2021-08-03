8252d9a2320fafee78c45b1128732fd86296cb9f
TheGioXD <83745961+TheGioXD@users.noreply.github.com>
2021-08-03 12:30:05 -0700

    Create set-msgdespedidas.js

---
+++ comandos/despedidas/set-msgdespedidas.js
@@ -0,0 +1,11 @@
+module.exports = ({
+    name: "set-msgdespedidas",
+    code: `
+<:tick:856950258219352075> | Mensaje de despedidas establecido correctamente!
+$setServerVar[leavemsg;$message]
+$onlyIf[$charCount[$message]<500;<:cross_red:856744445395206144> | El mensaje no debe exceder de 500 caracteres]
+$argsCheck[>1;<:cross_red:856744445395206144> | Mensaje no proporcionado]
+$onlyPerms[manageserver;<:cross_red:856744445395206144> | No cuentas con el permiso necesario]
+$cooldown[10s;<:cross_red:856744445395206144> | Tranquil@... \`%time%\`]
+$onlyIf[$getServerVar[despedidas]==true;<:cross_red:856744445395206144> | La categoria \`despedidas\` no esta activada. Uso: !activar despedidas]`
+})
