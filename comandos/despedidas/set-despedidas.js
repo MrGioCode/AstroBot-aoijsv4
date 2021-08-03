d7ce367665070a2a9df27f313e26e5af79c15371
TheGioXD <83745961+TheGioXD@users.noreply.github.com>
2021-08-03 12:29:37 -0700

    Create set-despedidas.js

---
+++ comandos/despedidas/set-despedidas.js
@@ -0,0 +1,12 @@
+module.exports = ({
+    name: "set-despedidas",
+    code: `
+<:tick:856950258219352075> | Registro de despedidas establecido correctamente!
+$setServerVar[leavech;$mentionedChannels[1]]
+$onlyIf[$channelType[$findServerChannel[$message[1];no]]==text;<:cross_red:856744445395206144> | Solo canales de texto]
+$onlyIf[$channelExists[$mentionedChannels[1]]==true;<:cross_red:856744445395206144> | ID de canal invalida]
+$argsCheck[>1;<:cross_red:856744445395206144> | ID no proporcionada]
+$onlyPerms[manageserver;<:cross_red:856744445395206144> | No cuentas con el permiso necesario]
+$cooldown[1m;<:cross_red:856744445395206144> | Tranquil@...]
+$onlyIf[$getServerVar[despedidas]==true;<:cross_red:856744445395206144> | La categoria \`despedidas\` no esta activada. Uso: !activar despedidas]`
+})
