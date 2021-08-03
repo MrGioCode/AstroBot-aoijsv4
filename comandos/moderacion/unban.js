bb7430ef6239ff898f5ab9aacdf4e0e31a27dde9
TheGioXD <83745961+TheGioXD@users.noreply.github.com>
2021-08-03 12:42:54 -0700

    Create unban.js

---
+++ comandos/moderacion/unban.js
@@ -0,0 +1,14 @@
+module.exports = ({
+    name: "unban",
+    code: `
+    <:tick:856950258219352075> | \`$userTag[$findUser[$message[1]]]\` desbaneado correctamente
+$unban[$findUser[$message[1]];$replaceText[$replaceText[$checkCondition[$messageSlice[1]==];true;No se menciono una razon.];false;$messageSlice[1]]]
+$onlyIf[$findUser[$message[1]]!=$authorID;<:cross_red:856744445395206144> | Tu ID no]
+$onlyIf[$findUser[$message[1]]!=$clientID;<:cross_red:856744445395206144> | Mi ID no]
+$onlyIf[$findUser[$message[1]]!=$ownerID;<:cross_red:856744445395206144> | ID no encontrada en la banlist]
+$onlyIf[$isBanned[$findUser[$message[1]]]==true;<:cross_red:856744445395206144> | ID no encontrada en la banlist]
+$argsCheck[>1;<:cross_red:856744445395206144> | Menciona al usuario que deseas desbanear]
+$onlyPerms[ban;<:cross_red:856744445395206144> | No cuentas con los permisos necesarios]
+$onlyBotPerms[ban;<:cross_red:856744445395206144> | No cuento con el permiso necesario]
+$onlyIf[$getServerVar[moderacion]==true;<:cross_red:856744445395206144> | Los comandos \`moderacion\` no estan activados. Usa: !activar moderacion]`
+})
