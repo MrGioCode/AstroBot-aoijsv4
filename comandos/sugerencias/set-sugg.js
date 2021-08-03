2132d2ad1a1c9fec4a803924338878266a53e907
TheGioXD <83745961+TheGioXD@users.noreply.github.com>
2021-08-03 12:50:31 -0700

    Create set-sugg.js

---
+++ comandos/sugerencias/set-sugg.js
@@ -0,0 +1,12 @@
+module.exports = ({
+    name: "set-sugg",
+    code: `
+<:tick:856950258219352075> | Canal de sugerencias establecido correctamente!
+$setServerVar[suggch;$mentionedChannels[1]]
+$onlyIf[$channelType[$mentionedChannels[1]]==text;<:cross_red:856744445395206144> | Solo canales de texto]
+$onlyif[$serverChannelExists[$mentinedChannels[1]]==false;<:cross_red:856744445395206144> | ID de canal invalida]
+$argsCheck[>1;<:cross_red:856744445395206144> | ID no porporcionado]
+$onlyPerms[manageserver;<:cross_red:856744445395206144> | No cuentas con el permiso necesario]
+$cooldown[3s;<:cross_red:856744445395206144> | Tranquil@...]
+$onlyIf[$getServerVar[sugerencias]==true;<:cross_red:856744445395206144> | La categoria \`sugerencias\` no esta activada. Uso: !activar sugerencias]`
+})
