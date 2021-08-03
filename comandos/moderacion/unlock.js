c8e6cfdb720ca8b3492140d27267175f936a0976
TheGioXD <83745961+TheGioXD@users.noreply.github.com>
2021-08-03 12:43:10 -0700

    Create unlock.js

---
+++ comandos/moderacion/unlock.js
@@ -0,0 +1,10 @@
+module.exports = ({
+  name: "unlock",
+  code: `
+<:tick:856950258219352075> | \`$channelName[$channelID]\` Desbloqueado correctamente
+$modifyChannelPerms[$channelID;+sendmessages;$guildID]
+$onlyPerms[managechannels;<:cross_red:856744445395206144> | No cuentas con los permisos necesarios]
+$onlyBotPerms[managechannels;<:cross_red:856744445395206144> | No cuento con el permiso necesario]
+$cooldown[1m;<:cross_red:856744445395206144> | Tranquilo...]
+$onlyIf[$getServerVar[moderacion]==true;<:cross_red:856744445395206144> | Los comandos \`moderacion\` no estan activados. Usa: !activar moderacion]`
+});
