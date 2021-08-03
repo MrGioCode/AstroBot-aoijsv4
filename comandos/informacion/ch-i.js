82abb824e399fa1868c4ca41d1921a092eb71f73
TheGioXD <83745961+TheGioXD@users.noreply.github.com>
2021-08-03 12:36:55 -0700

    Create ch-i.js

---
+++ comandos/informacion/ch-i.js
@@ -0,0 +1,16 @@
+module.exports = ({
+    name: "ch-i",
+    code: `
+$reply[$messageID;{author:Informacion #$channelName[$mentionedChannels[1]]:$authorAvatar}
+{description:
+**Nombre: [\`#$channelName[$mentionedChannels[1]]\`]**
+**Mencion: [<#$mentionedChannels[1]>]**
+**tema: [\`$channelTopic[$mentionedChannels[1]]\`]**
+**Tipo De Canal: [\`$channelType[$mentionedChannels[1]]\`]**}
+{color:ORANGE}
+{footer:$userTag $addTimestamp:$authorAvatar};no]
+$onlyIf[$serverChannelExists[$mentionedChannels[1]]==true;<:cross_red:856744445395206144> | El canal que se proporciono no fue encontrado]
+$argsCheck[>1;<:cross_red:856744445395206144> | Canal no proporcionado]
+$cooldown[3s;<:cross_red:856744445395206144> | Tranquilo...]
+$onlyIf[$getServerVar[informacion]==true;<:cross_red:856744445395206144> | Los comandos \`informativos\` no estan activados. Usa: !activar informacion]`
+})
