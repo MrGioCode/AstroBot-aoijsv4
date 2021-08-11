ac566f45d7550b4053d48480b7ead27c82cc4e6f
TheGioXD <83745961+TheGioXD@users.noreply.github.com>
2021-08-10 19:36:46 -0700

    Create banAdd.js

---
+++ comandos/registros/banAdd.js
@@ -0,0 +1,19 @@
+module.exports = ({
+  channel: "$getServerVar[logsch]",
+  code: `
+  $author[Usuario Baneado;$authorAvatar]
+  $description[
+    ==> **Informacion Del Ban** \`Log #$getServerVar[logsnum]\`
+
+    **Usuario**
+    > \`\`\`$userTag[$authorID]\`\`\`
+    **Responable**
+    > \`\`\`$userTag[$getAuditLogs[1;everyone;MEMBER_BAN_ADD;$guildID;{executor.id}]]\`\`\`
+    **Razon**
+    > \`\`\`$getBanReason[$authorID;$guildID]\`\`\`]
+    $thumbnail[$authorAvatar]
+    $footer[$userTag[$getAuditLogs[1;everyone;MEMBER_BAN_ADD;$guildID;{executor.id}]] $addTimestamp]
+    $color[RED]
+    $setServerVar[logsnum;$sum[$getServerVar[logsnum];1]]`,
+  type: 'banAddCommand'
+})
