c8643796fae8e44e0006a215a5c9516fa52f608f
TheGioXD <83745961+TheGioXD@users.noreply.github.com>
2021-08-10 19:37:40 -0700

    Create channelCreate.js

---
+++ comandos/registros/channelCreate.js
@@ -0,0 +1,19 @@
+module.exports = ({
+  channel: "$getServerVar[logsch]",
+  code: `
+  $author[Canal Creado;$userAvatar[$getAuditLogs[1;everyone;CHANNEL_CREATE;$guildID;{executor.id}]]]
+  $description[
+    ==> **Informacion Del Canal Creado** \`Log #$getServerVar[logsnum]\`
+
+    **Nombre**
+    > \`\`\`#$channelName[$newChannel[id]]\`\`\`
+    **Posicion**
+    > \`\`\`$newChannel[position]\`\`\`
+    **Creador**
+    > \`\`\`$userTag[$getAuditLogs[1;everyone;CHANNEL_CREATE;$guildID;{executor.id}]]\`\`\`]
+    $thumbnail[$userAvatar[$getAuditLogs[1;everyone;CHANNEL_CREATE;$guildID;{executor.id}]]]
+    $footer[$userTag[$getAuditLogs[1;everyone;CHANNEL_CREATE;$guildID;{executor.id}]] $addTimestamp]
+    $color[YELLOW]
+    $setServerVar[logsnum;$sum[$getServerVar[logsnum];1]]`,
+  type: 'channelCreateCommand'
+})
