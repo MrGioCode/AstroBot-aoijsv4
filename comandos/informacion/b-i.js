f516b91a7ca644576b0edf8690da9409440b290e
TheGioXD <83745961+TheGioXD@users.noreply.github.com>
2021-08-10 22:28:43 -0700

    Update b-i.js

--- comandos/informacion/b-i.js
+++ comandos/informacion/b-i.js
@@ -23,7 +23,7 @@ $reply[$messageID;{author:Informacion $userTag[$clientID]:$userAvatar[$clientID]
 **OS**
 > \`\`\`Ubuntu 20.04\`\`\`
 **Host**
-> \`\`\`VPS Linux\`\`\`
+> \`\`\`Railway\`\`\`
 **NodeJS**
 > \`\`\`v16.6.1\`\`\`
 **AoiJS**
