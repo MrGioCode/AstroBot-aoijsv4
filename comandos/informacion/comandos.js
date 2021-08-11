25dacb5e491c3931a448c72047e19b92d38f2ce5
TheGioXD <83745961+TheGioXD@users.noreply.github.com>
2021-08-10 20:58:48 -0700

    Update comandos.js

--- comandos/informacion/comandos.js
+++ comandos/informacion/comandos.js
@@ -1,7 +1,7 @@
 module.exports = ({
    name: "comandos",
    code: `
-$author[xAstroBot:$authorAvatar]
+$author[xAstroBot;$authorAvatar]
 $description[
 
 **Informacion**
@@ -22,7 +22,7 @@ $description[
 > __!help \`sug\`__
 
 <:link:856765531382218772> | [invitacion](https://discord.com/api/oauth2/authorize?client_id=864965981290102784&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.gg%2FkHv3dV3KeW&scope=bot) | [soporte](https://discord.gg/mKQGB6Q2VP) |]
-$footer[Lista de categorias $addTimestamp]
+$footer[Lista de categorias $addTimestamp;$authorAvatar]
 $thumbnail[$userAvatar[$clientID]]
 $color[WHITE]`
 })
