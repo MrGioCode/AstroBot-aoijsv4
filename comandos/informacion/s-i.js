e607d8a22588796d448668b45c0c729ddd0762f8
TheGioXD <83745961+TheGioXD@users.noreply.github.com>
2021-08-10 19:35:43 -0700

    Update s-i.js

--- comandos/informacion/s-i.js
+++ comandos/informacion/s-i.js
@@ -3,13 +3,22 @@ module.exports = ({
     code: `
 $reply[$messageID;{author:Informacion De Servidor:$authorAvatar}
 {description:
-**Creador: [<@$ownerID>]**
-**Nombre: [\`$serverName[$guildID]\`]**
-**ID: [\`$guildID\`]**
-**Localizacion: [\`$serverRegion\`]**
-**Total De Canales: [\`Texto - $channelCount[text]\`|\`Voz - $channelCount[voice]\`|\`Categorias - $channelCount[category]\`]**
-**Total De Roles y Miembros: [\`Roles - $roleCount\`|\`Miembros - $membersCount\`]**
-**Lista De roles: [\`@$guildRoles[name]\`]**}
+==> **Informacion Servidor \`$serverName[$guildID]\`**
+
+**Creador**
+> <@$ownerID>
+**Nombre**
+> \`$serverName[$guildID]\`
+**ID**
+> \`$guildID\`
+**Localizacion**
+> \`$serverRegion\`
+**Total De Canales**
+> \`Texto $channelCount[text] | Voz $channelCount[voice] | Categorias $channelCount[category]\`
+**Total De Miembros Y Roles**
+> \`Roles $roleCount | Miembros $membersCount | Bots $botCount\`
+**Roles**
+> $guildRoles[mention]}
 {color:ORANGE}
 {footer:$userTag $addTimestamp:$authorAvatar};no]
 $cooldown[3s;<:cross_red:856744445395206144> | Tranquilo...]
