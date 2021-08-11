e67a91af8d2c94568b1b7c11f25f9e5df7d2850a
TheGioXD <83745961+TheGioXD@users.noreply.github.com>
2021-08-10 19:37:13 -0700

    Create banRemove.js

---
+++ comandos/registros/banRemove.js
@@ -0,0 +1,17 @@
+module.exports = ({
+  channel: "$getServerVar[logsch]",
+  code: `
+  $author[Usuario Desbaneado;$authorAvatar]
+  $description[
+    ==> **Informacion Del Unban** \`Log #$getServerVar[logsnum]\`
+
+    **Usuario**
+    > \`\`\`$userTag[$authorID]\`\`\`
+    **Responable**
+    > \`\`\`$userTag[$getAuditLogs[1;everyone;MEMBER_BAN_REMOVE;$guildID;{executor.id}]]\`\`\`]
+    $thumbnail[$authorAvatar]
+    $footer[$userTag[$getAuditLogs[1;everyone;MEMBER_BAN_REMOVE;$guildID;{executor.id}]] $addTimestamp]
+    $color[GREEN]
+    $setServerVar[logsnum;$sum[$getServerVar[logsnum];1]]`,
+  type: 'banRemoveCommand'
+})
