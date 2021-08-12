5fa6d90da10cbb7df4a25bc8b3fe8e243460f096
TheGioXD <83745961+TheGioXD@users.noreply.github.com>
2021-08-11 20:18:50 -0700

    Update crear-rol.js

--- comandos/configuracion/crear-rol.js
+++ comandos/configuracion/crear-rol.js
@@ -8,8 +8,7 @@ $setServerVar[createrolen;$message]
 $argsCheck[>1;<:cross_red:856744445395206144> | Nombre no proporcionado]
 $onlyPerms[manageroles;<:cross_red:856744445395206144> | No cuentas con el permiso necesario]
 $onlyBotPerms[manageroles;<:cross_red:856744445395206144> | No cuento con el permiso necesario]
-$cooldown[3s;<:cross_red:824001307001487370> | Tranquilo...]
-$onlyIf[$getServerVar[configuracion]==true;<:cross_red:856744445395206144> | La categoria \`configuracion\` no esta activada. Uso: !activar configuracion]`
+$cooldown[3s;<:cross_red:824001307001487370> | Tranquilo...]`
 }, {
     name: "createrole1",
 	code: `
