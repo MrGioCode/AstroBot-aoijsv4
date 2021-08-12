df60395c0fdf71a2deae4d6243f7c7975c45f90b
TheGioXD <83745961+TheGioXD@users.noreply.github.com>
2021-08-11 20:17:26 -0700

    Update set-msgbienvenidas.js

--- comandos/bienvenidas/set-msgbienvenidas.js
+++ comandos/bienvenidas/set-msgbienvenidas.js
@@ -6,6 +6,5 @@ $setServerVar[welcomemsg;$message]
 $onlyIf[$charCount[$message]<500;<:cross_red:856744445395206144> | El mensaje no debe exceder de 500 caracteres]
 $argsCheck[>1;<:cross_red:856744445395206144> | Mensaje no proporcionado]
 $onlyPerms[manageserver;<:cross_red:856744445395206144> | No cuentas con el permiso necesario]
-$cooldown[10s;<:cross_red:856744445395206144> | Tranquil@... \`%time%\`]
-$onlyIf[$getServerVar[bienvenidas]==true;<:cross_red:856744445395206144> | La categoria \`bienvenidas\` no esta activada. Uso: !activar bienvenidas]`
+$cooldown[10s;<:cross_red:856744445395206144> | Tranquil@... \`%time%\`]`
 })
