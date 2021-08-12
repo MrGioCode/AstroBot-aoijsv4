d7ccafa9b2f32fd90b71eed0803b95b131baa0e0
TheGioXD <83745961+TheGioXD@users.noreply.github.com>
2021-08-11 20:21:57 -0700

    Update set-autorole.js

--- comandos/establecer/set-autorole.js
+++ comandos/establecer/set-autorole.js
@@ -6,8 +6,7 @@ $setServerVar[autorole]
 $onlyIf[$roleExists[$mentionedRoles[1]]==true;<:cross_red:856744445395206144> | ID rol invalido]
 $argsCheck[>1;<:cross_red:856744445395206144> | Rol no proporcionado]
 $onlyPerms[admin;<:cross_red:856744445395206144> | No cuentas con el permiso necesario]
-$cooldown[3s;<:cross_red:856744445395206144> | Tranquilo...]
-$onlyIf[$getServerVar[establecer]==true;<:cross_red:856744445395206144> | La categoria \`establecer\` no esta activada. Uso: !activar establecer]`
+$cooldown[3s;<:cross_red:856744445395206144> | Tranquilo...]`
 }, {
     channel: "$randomChannelID",
     code: `
