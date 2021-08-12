13a597e93a601efabd5e40f590891c8cb533364f
TheGioXD <83745961+TheGioXD@users.noreply.github.com>
2021-08-11 20:31:02 -0700

    Update set-logs.js

--- comandos/seguridad/set-logs.js
+++ comandos/seguridad/set-logs.js
@@ -6,6 +6,5 @@ $setServerVar[logsch;$mentionedChannels[1]]
 $onlyIf[$channelType[$mentionedChannels[1]]==text;<:cross_red:856744445395206144> | Solo canales de texto]
 $onlyIf[$serverChannelExists[$mentionedChannels[1]]==false;<:cross_red:856744445395206144> | ID de canal invalido]
 $onlyPerms[manageserver;<:cross_red:856744445395206144> | No cuentas con el permiso necesarios]
-$cooldown[3s;<:cross_red:856744445395206144> | Tranquil@...]
-$onlyIf[$getServerVar[seguridad]==true;<:cross_red:856744445395206144> | La categoria \`seguridad\` no esta activada. Uso: !activar seguridad]`
+$cooldown[3s;<:cross_red:856744445395206144> | Tranquil@...]`
 })
