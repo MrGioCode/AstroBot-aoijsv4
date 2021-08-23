4413158bca5205409c447451be49cd76b6c36f2e
TheGioXD <castrogiovanni281@gmail.com>
2021-08-23 10:34:02 -0700

    yea

---
+++ comandos/diversion/say.js
@@ -0,0 +1,8 @@
+module.exports = ({
+  name: "say",
+  code: `
+$message
+$disableEveryoneMentions
+$onlyIf[$checkContains[$message;@]==true;<:cross_red:856744445395206144> | Se bloqueo el uso de algunos caracteres]
+$onlyIf[$checkContains[$message;discord.gg;dsc.gg;https://]==true;<:cross_red:856744445395206144> | No enlaces]`
+})
