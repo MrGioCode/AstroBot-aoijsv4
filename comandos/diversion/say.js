18512b51a5ecf74383c2c177fb0eed916636ffa1
TheGioXD <castrogiovanni281@gmail.com>
2021-08-23 10:50:37 -0700

    oka

--- comandos/diversion/say.js
+++ comandos/diversion/say.js
@@ -3,6 +3,6 @@ module.exports = ({
   code: `
 $message
 $disableEveryoneMentions
-$onlyIf[$checkContains[$message;@]==true;<:cross_red:856744445395206144> | Se bloqueo el uso de algunos caracteres]
+$onlyIf[$checkContains[$message;@]==false;<:cross_red:856744445395206144> | Se bloqueo el uso de algunos caracteres]
 $onlyIf[$checkContains[$message;discord.gg;dsc.gg;https://]==false;<:cross_red:856744445395206144> | No enlaces]`
 })
