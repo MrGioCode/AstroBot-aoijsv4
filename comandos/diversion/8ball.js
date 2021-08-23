38bd82eafaffbeab6af4740def876fe99498d3d2
TheGioXD <castrogiovanni281@gmail.com>
2021-08-23 10:26:57 -0700

    Actualizando

---
+++ comandos/diversion/8ball.js
@@ -0,0 +1,14 @@
+module.exports = ({
+  name: "8ball",
+  code: `
+$author[8ball!! 🎱;$authorAvatar]
+$description[
+**Pregunta**
+> $message
+**Respuesta**
+> \`$randomText[si;no;si.;no.;que;bruh;nose;.;brutal;si hazlo;no lo hagas;pero;no me interesa]\`]
+$footer[$userTag[$authorID];$authorID]
+$color[BLUE]
+$onlyIf[$charCount[$message]<20;<:cross_red:856744445395206144> | Maximo \`10\` caracteres]
+$cooldown[3s;<:cross_red:856744445395206144> | Tranquil@...]`
+})
--- comandos/informacion/comandos.js
+++ comandos/informacion/comandos.js
@@ -31,6 +31,12 @@ $description[
 > 📮 | **Establecer**
 \`set-mute\`, \`set-autorole\`, \`set-logs\`.
 
+> 🎉 | **Giveaways**
+\`coming soon...\`.
+
+> 🎈 | **Diversión**
+\`8ball\`, \`say\`.
+
 <:link:856765531382218772> | [invitacion](https://discord.com/api/oauth2/authorize?client_id=864965981290102784&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.gg%2FkHv3dV3KeW&scope=bot) | [soporte](https://discord.gg/mKQGB6Q2VP) |]
 $footer[Lista de categorias $addTimestamp;$authorAvatar]
 $thumbnail[$userAvatar[$clientID]]
