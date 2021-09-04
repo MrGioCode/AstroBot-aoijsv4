9201c052ce5c2b5f9dfbd1e55fcd530f16b337ec
TheGioXD <castrogiovanni281@gmail.com>
2021-09-04 20:52:23 +0000

    actualizando

--- comandos/diversion/8ball.js
+++ comandos/diversion/8ball.js
@@ -7,7 +7,7 @@ $description[
 > $message
 **Respuesta**
 > \`$randomText[si;no;si.;no.;que;bruh;nose;.;brutal;si hazlo;no lo hagas;pero;no me interesa]\`]
-$footer[$userTag[$authorID];$authorID]
+$footer[$userTag[$authorID];$authorAvatar]
 $color[BLUE]
 $onlyIf[$charCount[$message]<20;<:cross_red:856744445395206144> | Maximo \`10\` caracteres]
 $cooldown[3s;<:cross_red:856744445395206144> | Tranquil@...]`
