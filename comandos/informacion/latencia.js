b127506cf33970c12263c1a191f93e7cb1533ceb
TheGioXD <83745961+TheGioXD@users.noreply.github.com>
2021-10-01 12:32:56 -0700

    Update latencia.js

--- comandos/informacion/latencia.js
+++ comandos/informacion/latencia.js
@@ -2,9 +2,9 @@ module.exports = ({
     name: "latencia",
     code: `
 $reply[$messageID;{author:Pong! 🏓:$authorAvatar}{description:
-**Tu Ping**
+**Websocket**
 > \`\`\`$pingms\`\`\`
-**Mi Ping**
+**Cliente**
 > \`\`\`$botPingms\`\`\`}{thumbnail:$userAvatar[$clientID]}{footer:$userTag $addTimestamp}{color:BLUE};no]
 $cooldown[3s;<:cross_red:856744445395206144> | Tranquilo...]`
 })
